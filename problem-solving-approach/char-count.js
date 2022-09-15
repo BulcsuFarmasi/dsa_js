8bfab327863bb247196f9f167beea7f59ba3e5a6
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-09-15 15:25:36 +0200

    use function instead of regex

--- problem-solving-approach/char-count.js
+++ problem-solving-approach/char-count.js
@@ -2,11 +2,21 @@ function charCount(str) {
     const result = {}
     for (let char of str) {
         char = char.toLowerCase();
-        if (/[a-z0-9]/.test(char)) {
+        if (isAlphaNumeric(char)) {
             result[char] = ++result[char] || 1;
         }
     }
     return result;
+}
+
+function isAlphaNumeric(char) {
+    const code = char.charCodeAt(0);
+    if (!(code > 47 && code < 58) && 
+        !(code > 64  && code < 91) && 
+        !(code > 96 && code < 123)) {
+            return false;
+        }
+    return true;
 }
 
 console.log(charCount("Twelve o'Clock"));
