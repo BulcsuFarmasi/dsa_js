4e577702a62276e7171feecd51757999bdf47a5a
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-09-15 14:57:13 +0200

    use one instead of an if else

--- problem-solving-approach/char-count.js
+++ problem-solving-approach/char-count.js
@@ -1,13 +1,9 @@
 function charCount(str) {
     const result = {}
-    for (let i = 0; i < str.length; i++) {
-        const char = str[i].toLowerCase();
+    for (let char of str) {
+        char = char.toLowerCase();
         if (/[a-z0-9]/.test(char)) {
-            if (result[char] > 0) {
-                result[char]++;
-            } else {
-                result[char] = 1;
-            }
+            result[char] = ++result[char] || 1;
         }
     }
     return result;
