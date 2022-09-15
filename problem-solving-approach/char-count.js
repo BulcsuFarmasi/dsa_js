d069794ef4705b156402c4d19d61b553eeafa514
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-09-15 13:54:46 +0200

    char count initial

---
+++ problem-solving-approach/char-count.js
@@ -0,0 +1,14 @@
+function charCount(str) {
+    const result = {}
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        if (result[char] > 0) {
+            result[char]++;
+        } else {
+            result[char] = 1;
+        }
+    }
+    return result;
+}
+
+console.log(charCount("Twelve o'Clock"));
