ccab955eaef3dc66ab45c272f5de41145de05abc
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-09-16 12:21:54 +0200

    sume zero with pointers

--- problem-solving-patterns/sum-zero.js
+++ problem-solving-patterns/sum-zero.js
@@ -1,12 +1,16 @@
 function sumZero(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] + arr[j] === 0) {
-                return[arr[i],arr[j]]
-            }
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+        if (sum === 0) {
+            return[arr[left], arr[right]];
+        } else if (sum > 0) {
+            right--;
+        } else {
+            left++;
         }
-        
     }
 }
 
-console.log(sumZero([-10,5,6,7]));
+console.log(sumZero([-10,-7,-6,5,7,8,11]));
