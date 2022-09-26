e40498e888e6129559cf40b61b9e85832467a5ce
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-09-26 15:21:58 +0200

    set j to 1 to spare one cycle

--- problem-solving-patterns/count-unique-values.js
+++ problem-solving-patterns/count-unique-values.js
@@ -5,10 +5,10 @@ function countUniqueValues(array) {
 
     let i = 0;
 
-    for (let j = 0; j < array.length; j++) {
+    for (let j = 1; j < array.length; j++) {
         if (array[i] !== array[j]) {
-            array[i] = array[j];
             i++;
+            array[i] = array[j];
         }
     }
 
