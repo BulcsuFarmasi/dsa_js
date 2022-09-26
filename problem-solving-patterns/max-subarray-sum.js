754729cdf838eb81a8d892b381216f3f56ddae60
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-09-26 16:16:43 +0200

    max subarray sum uing sliding window

--- problem-solving-patterns/max-subarray-sum.js
+++ problem-solving-patterns/max-subarray-sum.js
@@ -1,19 +1,17 @@
 function maxSubarraySum(arr, num) {
-    if (num > arr.length) {
-        return null;
-    }
-    let max = -Infinity;
-    for (let i = 0; i < arr.length - num + 1; i++) {
-        let temp = 0;
-        for (let j = 0; j < num; j++) {
-            temp += arr[i + j];
-        }
-        if (temp > max) {
-            max = temp
-        }
+    let maxSum = 0;
+    let tempSum = 0;
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i];
     }
 
-    return max;
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+
+    return maxSum;
 }
 
 console.log(maxSubarraySum([3,1,2,5,6],3));
