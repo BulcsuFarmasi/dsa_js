f96f98d97e9444c581601bcc831f8fab98f80d51
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-09-27 11:03:01 +0200

    use binary search for search

--- problem-solving-patterns/search.js
+++ problem-solving-patterns/search.js
@@ -1,10 +1,18 @@
 function search(arr, val) {
-    for (let  i= 0; i < arr.length; i++) {
-        if (arr[i] === val) {
-            return i;
+    let min = 0;
+    let max = arr.length - 1;
+    while (min <= max) {
+        let middle = Math.floor((min + max) / 2);
+        let currentElement = arr[middle];
+        if (currentElement < val) {
+            min = middle + 1;
+        } else if (currentElement > val) {
+            max = middle - 1;
+        } else {
+            return middle;
         }
     }
     return -1;
 }
 
-console.log(search([10,4,40,1], 11));
+console.log(search([10,12,54,111], 111));
