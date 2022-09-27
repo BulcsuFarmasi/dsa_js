cff38625d14f588001b7fc3a6d3e31178e996fe8
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-09-27 10:50:35 +0200

    fill loop body

--- problem-solving-patterns/search.js
+++ problem-solving-patterns/search.js
@@ -1,5 +1,10 @@
 function search(arr, val) {
     for (let  i= 0; i < arr.length; i++) {
-        
+        if (arr[i] === val) {
+            return i;
+        }
     }
+    return -1;
 }
+
+console.log(search([10,4,40,1], 11));
