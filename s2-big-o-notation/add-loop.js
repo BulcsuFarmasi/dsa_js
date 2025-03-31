3bee5f9344d3500d24a99e71cf233600858b656a
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2025-03-31 23:08:42 +0200

    add measuring tool

--- s2-big-o-notation/add-loop.js
+++ s2-big-o-notation/add-loop.js
@@ -7,4 +7,8 @@ function addUpTo(n) {
     return total;
 }
 
-console.log(addUpTo(100));
+let t1 = performance.now()
+addUpTo(1000000000)
+let t2 = performance.now()
+
+console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds`)
