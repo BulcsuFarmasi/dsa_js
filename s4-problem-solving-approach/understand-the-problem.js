0e6a5c9bffa581e47302e2070dec0b8f1e821dd2
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2025-06-23 20:01:49 +0200

    fix typo

--- s4-problem-solving-approach/understand-the-problem.js
+++ s4-problem-solving-approach/understand-the-problem.js
@@ -7,7 +7,7 @@
 /// 3. What are the outputs that should come from the solution to the problem?
 ///
 
-/// 4. Can the ouputs be determined from the outputs? In other words, do I have enough information to solve the problem
+/// 4. Can the outputs be determined from the outputs? In other words, do I have enough information to solve the problem
 ///
 
 /// 5. How should I label the important pieces of data that are part of the problem?
