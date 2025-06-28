a4debec5f9cbbaeb011c5354c9be1bbf432d00fa
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2025-06-28 20:26:37 +0200

    add basic loop

--- s4-problem-solving-approach/break-it-down.js
+++ s4-problem-solving-approach/break-it-down.js
@@ -1,5 +1,7 @@
 function charCount(str) {
     // make object to return at end
-    // loop over string
+    // loop over string for each character
+        // if char is a key in object, add one to count
+        // if char is not in object add it and set value to 1  
     // return object at end
 }
