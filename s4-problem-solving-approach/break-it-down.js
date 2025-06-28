92ccd3bdc698818e980cea679172ca9500febe80
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2025-06-28 20:16:32 +0200

    break it down very basic

---
+++ s4-problem-solving-approach/break-it-down.js
@@ -0,0 +1,4 @@
+function charCount(str) {
+    // do something
+    // return an object with keys that are lowercase alphanumeric characters in the strings; values should be the counts for those characters
+}
